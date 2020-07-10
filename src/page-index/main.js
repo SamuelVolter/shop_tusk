eb196387c951a23fc5e2cc49346d6328f6ee58a0
SamuelVolter <65598951+SamuelVolter@users.noreply.github.com>
2020-07-10 22:01:47 +0300

    upadte

--- src/page-index/main.js
+++ src/page-index/main.js
@@ -1,8 +1,10 @@
 import { Bar } from '../js/bar.es6'
+
 const { Foo } = require('../js/foo.cjs')
 require('normalize.css/normalize.css')
 require('../css/main.css')
 require('./page.css')
+require('../scss/base.scss')
 
 document.addEventListener('DOMContentLoaded', () => {
   console.log('DOMContentLoaded', 'page-index')
@@ -13,3 +15,11 @@ window.Bar = Bar()
 
 console.log(window.Foo.getValue())
 console.log(window.Bar)
+// BURHER
+document.getElementById("trigger").onclick = function() {
+  open()
+};
+
+function open() {
+  document.getElementById("menu").classList.toggle("show");
+}
--- webpack.dev.js
+++ webpack.dev.js
@@ -31,10 +31,11 @@ module.exports = {
         }
       },
       {
-        test: /\.css$/i,
+        test: /\.(css|scss)(\?.*)?$/i,
         use: [
           'style-loader',
-          'css-loader'
+          'css-loader',
+          'sass-loader'
           // Please note we are not running postcss here
         ]
       },
